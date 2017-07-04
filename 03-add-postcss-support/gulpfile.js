91f9e2323ce6716b82ae020792d235290cc64f27
Andrew Siemer <andrewsiemer@gmail.com>
2017-07-04 14:57:51 -0500

    added autoprefixer and source maps to build

--- 03-add-postcss-support/gulpfile.js
+++ 03-add-postcss-support/gulpfile.js
@@ -8,5 +8,17 @@ gulp.task('hello', function() {
 gulp.task('sass', function() {
 	return gulp.src('app/static/scss/styles.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('dist/css'));
+		.pipe(gulp.dest('build/css'));
 })
+
+gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('build/css/styles.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/css'));
+});
